67b8b628a3565213e237e45dbdac64b7af60d101
Brandon Roberts <Brandon.Roberts@sagefreight.com>
2023-09-06 13:01:01 -0400

    Minor cleanup.

--- Recipes_MediatR_API/Program.cs
+++ Recipes_MediatR_API/Program.cs
@@ -6,15 +6,11 @@ var connectionString = builder.Configuration.GetConnectionString("Recipes") ?? "
 builder.Services.AddSqlite<RecipeDb>(connectionString);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -22,10 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-//app.MapControllers();
 
 app.MapGet("/recipes", async (RecipeDb db) =>
 {
