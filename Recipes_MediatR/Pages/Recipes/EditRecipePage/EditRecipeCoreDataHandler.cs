875e021424d6cf598ad2dfaac323954b8ce0ba9a
Brandon Roberts <Brandon.Roberts@sagefreight.com>
2023-09-15 10:11:54 -0400

    Working validation.

--- Recipes_MediatR/Pages/Recipes/EditRecipePage/EditRecipeCoreDataHandler.cs
+++ Recipes_MediatR/Pages/Recipes/EditRecipePage/EditRecipeCoreDataHandler.cs
@@ -1,9 +1,29 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Recipes_MediatR.Config;
 using System.Net.Http.Json;
 
 namespace Recipes_MediatR.Pages.Recipes.EditRecipePage
 {
+    public class EditRecipeCoreDataValidator : AbstractValidator<GetRecipeCoreData.Response>
+    {
+        public EditRecipeCoreDataValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty();
+            RuleFor(x => x.CreatedDate)
+                .NotEmpty();
+            RuleFor(x => x.CreatedBy)
+                .NotEmpty();
+        }
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<GetRecipeCoreData.Response>.CreateWithOptions((GetRecipeCoreData.Response)model, x => x.IncludeProperties(propertyName)));
+            return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+
     public record EditRecipeCoreData
     {
         public record Request(int Id, string Title, DateTime? CreatedDate, string CreatedBy) : BlazorMediatRRequest, IRequest;
